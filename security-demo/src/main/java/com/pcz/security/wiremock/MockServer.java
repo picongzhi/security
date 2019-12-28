891da56a0acac7884d6a5cdbf6606e0574976489
picongzhi <picongzhi@gmail.com>
2019-12-28 13:11:36 +0800

    feat(wiremock): add wiremock

---
+++ security-demo/src/main/java/com/pcz/security/wiremock/MockServer.java
@@ -0,0 +1,28 @@
+package com.pcz.security.wiremock;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * @author picongzhi
+ */
+public class MockServer {
+    public static void main(String[] args) throws Exception {
+        WireMock.configureFor(8000);
+        WireMock.removeAllMappings();
+
+        mock("/order/1", "01.txt");
+        mock("/order/2", "02.txt");
+    }
+
+    private static void mock(String url, String filename) throws Exception {
+        ClassPathResource classPathResource = new ClassPathResource("mock/response/" + filename);
+        String content = FileUtils.readFileToString(classPathResource.getFile(), "UTF-8");
+
+        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo(url))
+                .willReturn(WireMock.aResponse()
+                        .withBody(content)
+                        .withStatus(200)));
+    }
+}
